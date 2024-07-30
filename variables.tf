914a1a9653482073abebd9eac7763724a9d96645
zloygagarko <zloygagarko12@gmail.com>
2024-07-29 19:52:28 -0500

    first

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "name" {
+  type = string
+  default = ""
+  description = "Please replace default value"
+}
+
+variable "type" {
+  type = string
+  default = ""
+  description = "Please replace with your record type"  
+}
+
+variable "env" {
+  type = string
+  default = ""
+  description = "subdomain env name"
+}
